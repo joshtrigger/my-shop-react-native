0a886100124a0de9cd5ab4c0d2c39982f6d36f8f
joshtrigger <lugjosh@gmail.com>
2019-10-02 21:00:49 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hey, my name is joshtrigger_</Text>
+      <Text>This is my very first react native app</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
